920ceecc2fd8f06060a59f1846222311aa99692f
Restyled.io <commits@restyled.io>
2021-12-19 14:02:19 +0000

    Restyled by prettier

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,62 +1,64 @@
 import Grid from "@material-ui/core/Grid";
-import {MDBBtn} from "mdbreact";
+import { MDBBtn } from "mdbreact";
 import React from "react";
-import {FaChevronRight} from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
 const Hero = ({ header, subtext, link, buttonText }) => (
   <section>
     <Grid container alignItems="center" className="hero-bg">
       <div className="hero-blur">
         <Grid
-container
-item
-direction = "row"
-style = {
-  {
-    display: "flex", alignContent: "center", justifyContent: "center",
-        padding: 0,
-  }
-} justify = "center"
-alignItems =
-    "center" > <Grid item xs = {12} lg = {12} sm = {12}>
-    <h2 className = "hero-header mt-4">Deployment
-        Test</h2>
+          container
+          item
+          direction="row"
+          style={{
+            display: "flex",
+            alignContent: "center",
+            justifyContent: "center",
+            padding: 0,
+          }}
+          justify="center"
+          alignItems="center"
+        >
+          {" "}
+          <Grid item xs={12} lg={12} sm={12}>
+            <h2 className="hero-header mt-4">Deployment Test</h2>
             <h2 className="hero-header mt-4">{header}</h2>
-    <p className = "hero-subtitle mt-4"><b>{subtext}</b>
+            <p className="hero-subtitle mt-4">
+              <b>{subtext}</b>
               <br></br>
-    <br>
-    </br>
-                <MDBBtn
-                  href={link}
-                  rounded
-                  floating
-                  size="lg"
-                  style={{
-                    marginTop: 60,
-                    backgroundColor:"white",
-                    color:"#22D3C1",
-                    borderRadius:100,
-                    boxShadow:"0px 8px 15px rgba(0, 0, 0, 0.1)"
-                  }}
-                  >
-                  {buttonText}
-                  <FaChevronRight size={24} style={{ marginRight: "5px" }} />
-    </MDBBtn>
-            </p>< p
-style =
-{
-  { marginLeft: "auto", marginRight: "auto" }
-} className =
-    "hero-button mt-4" > <br>
-    </br>
-              <div className="text-align"></div></p>
+              <br></br>
+              <MDBBtn
+                href={link}
+                rounded
+                floating
+                size="lg"
+                style={{
+                  marginTop: 60,
+                  backgroundColor: "white",
+                  color: "#22D3C1",
+                  borderRadius: 100,
+                  boxShadow: "0px 8px 15px rgba(0, 0, 0, 0.1)",
+                }}
+              >
+                {buttonText}
+                <FaChevronRight size={24} style={{ marginRight: "5px" }} />
+              </MDBBtn>
+            </p>
+            <p
+              style={{ marginLeft: "auto", marginRight: "auto" }}
+              className="hero-button mt-4"
+            >
+              {" "}
+              <br></br>
+              <div className="text-align"></div>
+            </p>
           </Grid>
-
-    <Grid item xs = {12} sm = {6} className = "hero-video mt-4">
-    </Grid>
-        </Grid></div>
-    </Grid>{" "} <
-    /section>
+          <Grid item xs={12} sm={6} className="hero-video mt-4"></Grid>
+        </Grid>
+      </div>
+    </Grid>{" "}
+  </section>
 );
 
 export default Hero;
