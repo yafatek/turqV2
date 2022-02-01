a8d112192d7e4e2e0f7da888e6dc55aa01900ce5
Restyled.io <commits@restyled.io>
2022-02-01 03:19:41 +0000

    Restyled by prettier

--- src/pages/legislationEditor.js
+++ src/pages/legislationEditor.js
@@ -1,41 +1,41 @@
-import 'firebase/database';
+import "firebase/database";
 
-import DiffMatchPatch from 'diff-match-patch';
-import firebase from 'firebase/app';
-import Firepad from 'firepad';
-import React from 'react';
-import ReactDiffViewer, {DiffMethod} from 'react-diff-viewer';
-import {connect} from 'react-redux';
+import DiffMatchPatch from "diff-match-patch";
+import firebase from "firebase/app";
+import Firepad from "firepad";
+import React from "react";
+import ReactDiffViewer, { DiffMethod } from "react-diff-viewer";
+import { connect } from "react-redux";
 
-import {updateLegislation} from '../actions/legislationActions';
-import Editor, {LeftPanel, RightPanel} from '../components/editor/editor';
-import EditorLayout from '../components/editor/layout';
-import LegislationText from '../components/legislation/legislationText';
+import { updateLegislation } from "../actions/legislationActions";
+import Editor, { LeftPanel, RightPanel } from "../components/editor/editor";
+import EditorLayout from "../components/editor/layout";
+import LegislationText from "../components/legislation/legislationText";
 
 class LegislationEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {ref : '', title : '', content : ''};
+    this.state = { ref: "", title: "", content: "" };
     this.handleSubmit = this._handleSubmit.bind(this);
     this.onSynced = this._onSynced.bind(this);
     this.onReady = this._onReady.bind(this);
-    this.email = localStorage.getItem('email');
+    this.email = localStorage.getItem("email");
     this.firepad = null;
-    this.originalText = '';
+    this.originalText = "";
     this.diffedContent = null;
   }
 
   componentDidMount() {
     const config = {
-      apiKey : 'AIzaSyCEQ7cLJuyrbRLq_A-sjXUwxseMaKVosEE',
-      authDomain : 'turq-legislation-editor-1.firebaseapp.com',
-      databaseURL :
-          'https://turq-legislation-editor-1-default-rtdb.firebaseio.com',
-      projectId : 'turq-legislation-editor-1',
-      storageBucket : 'turq-legislation-editor-1.appspot.com',
-      messagingSenderId : '670875815290',
-      appId : '1:670875815290:web:a5da8f93c91052ed44dac3',
-      measurementId : 'G-Y6NX5XHT68',
+      apiKey: "AIzaSyCEQ7cLJuyrbRLq_A-sjXUwxseMaKVosEE",
+      authDomain: "turq-legislation-editor-1.firebaseapp.com",
+      databaseURL:
+        "https://turq-legislation-editor-1-default-rtdb.firebaseio.com",
+      projectId: "turq-legislation-editor-1",
+      storageBucket: "turq-legislation-editor-1.appspot.com",
+      messagingSenderId: "670875815290",
+      appId: "1:670875815290:web:a5da8f93c91052ed44dac3",
+      measurementId: "G-Y6NX5XHT68",
     };
 
     if (!firebase.apps.length) {
@@ -43,24 +43,27 @@ class LegislationEditor extends React.Component {
     }
     const firepadRef = this._createRef();
     const codeMirror = window.CodeMirror(
-        document.getElementById('firepad-container'), {lineWrapping : true});
+      document.getElementById("firepad-container"),
+      { lineWrapping: true }
+    );
     this.firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
-      richTextShortcuts : true,
-      richTextToolbar : true,
+      richTextShortcuts: true,
+      richTextToolbar: true,
     });
-    this.firepad.on('ready', this.onReady);
-    this.firepad.on('synced', this.onSynced);
+    this.firepad.on("ready", this.onReady);
+    this.firepad.on("synced", this.onSynced);
   }
 
   // Add the text to state when the page initally loads
   _onReady() {
     const text = this.firepad.getText();
-    this.setState({...this.state, content : text, originalText : text});
-    if (this.firepad.getHtml() === '') {
+    this.setState({ ...this.state, content: text, originalText: text });
+    if (this.firepad.getHtml() === "") {
       this.firepad.setHtml(
-          '<div><b>Legislation Title</b></div><div><b></b><br/></div><div><b>Section 1</b></div><div><b></b><br/></div><div><b>Section 2&nbsp;</b></div><div><b></b><br/></div><div><b>Etc.</b></div>');
+        "<div><b>Legislation Title</b></div><div><b></b><br/></div><div><b>Section 1</b></div><div><b></b><br/></div><div><b>Section 2&nbsp;</b></div><div><b></b><br/></div><div><b>Etc.</b></div>"
+      );
     }
-    this.setState({...this.state, content : text});
+    this.setState({ ...this.state, content: text });
   }
 
   // Update the text stored in state everytime the user changes and text and the
@@ -68,7 +71,7 @@ class LegislationEditor extends React.Component {
   _onSynced(isSynced) {
     if (isSynced) {
       const text = this.firepad.getText();
-      this.setState({...this.state, content : text});
+      this.setState({ ...this.state, content: text });
       this._onDiff();
     }
   }
@@ -82,22 +85,25 @@ class LegislationEditor extends React.Component {
     const diff = dmp.diff_main(text1, text2);
     const prettyDiff = dmp.diff_prettyHtml(diff);
 
-    this.setState({diffedContent : prettyDiff});
+    this.setState({ diffedContent: prettyDiff });
   }
 
   // Create the reference to the firepad document -- /posts/<email>/<contestId>
   _createRef() {
     let ref = firebase.database().ref();
-    const URLRef = new URLSearchParams(this.props.location.search).get('ref');
+    const URLRef = new URLSearchParams(this.props.location.search).get("ref");
     if (URLRef) {
       ref = ref.child(URLRef);
     } else {
       ref = ref.push(); // generate unique location.
       const oldURL = window.location.origin + window.location.pathname;
-      window.history.replaceState(null, 'New Page Title',
-                                  `${oldURL}?ref=${ref.key}`);
+      window.history.replaceState(
+        null,
+        "New Page Title",
+        `${oldURL}?ref=${ref.key}`
+      );
     }
-    this.setState({...this.state, ref : ref.key});
+    this.setState({ ...this.state, ref: ref.key });
     return ref;
   }
 
@@ -110,14 +116,15 @@ class LegislationEditor extends React.Component {
     if (!this._checkMandatoryFields()) {
       return;
     }
-    const contest =
-        new URLSearchParams(this.props.location.search).get('contest');
+    const contest = new URLSearchParams(this.props.location.search).get(
+      "contest"
+    );
     const legislationId = this.props.match.params.id;
     const data = {
-      title : 'Submitted Bill',
-      ref : this.state.ref,
-      contestId : contest,
-      content : this.state.content,
+      title: "Submitted Bill",
+      ref: this.state.ref,
+      contestId: contest,
+      content: this.state.content,
     };
     this.props.dispatch(updateLegislation(legislationId, data));
   }
@@ -125,35 +132,36 @@ class LegislationEditor extends React.Component {
   render() {
     return (
       <EditorLayout onSubmit={this.handleSubmit}>
-        {!this.props.isFetching
-          ? (
-            <div>
-              <div className="row">
-                <div className="col">
-                  <Editor>
-                    <LeftPanel>
-                      <div className="my-3 mx-5">
-                        <div id="firepad-container" />
-                      </div>
-                    </LeftPanel>
-                    <RightPanel>
-                      <LegislationText
-                        title={this.state.title}
-                        content={
-      this.state.content}
-                      />
-                    </RightPanel>
-                  </Editor>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <div dangerouslySetInnerHTML={{ __html: this.state.diffedContent }} />
-                </div>
+        {!this.props.isFetching ? (
+          <div>
+            <div className="row">
+              <div className="col">
+                <Editor>
+                  <LeftPanel>
+                    <div className="my-3 mx-5">
+                      <div id="firepad-container" />
+                    </div>
+                  </LeftPanel>
+                  <RightPanel>
+                    <LegislationText
+                      title={this.state.title}
+                      content={this.state.content}
+                    />
+                  </RightPanel>
+                </Editor>
               </div>
             </div>
-          )
-          : <div />}
+            <div className="row">
+              <div className="col">
+                <div
+                  dangerouslySetInnerHTML={{ __html: this.state.diffedContent }}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div />
+        )}
       </EditorLayout>
     );
   }
