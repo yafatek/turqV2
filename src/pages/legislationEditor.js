0175feaa2dc85d239749f988cf2ced05d3065dc9
Jesse Cohn <cohnjesse@gmail.com>
2022-02-14 08:54:42 -0800

    Added tiny MCE

--- src/pages/legislationEditor.js
+++ src/pages/legislationEditor.js
@@ -1,223 +1,53 @@
-import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import firebase from 'firebase/app';
+import React, { Component, useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import 'firebase/database';
-import Firepad from 'firepad';
-import DiffMatchPatch from 'diff-match-patch';
-import { Button, TextField } from "@material-ui/core"
-import { jsPDF } from "jspdf";
-
-import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer';
 import EditorLayout from '../components/editor/layout';
-import LegislationText from '../components/legislation/legislationText';
-import Editor, { LeftPanel, RightPanel } from '../components/editor/editor';
-import { updateLegislation } from '../actions/legislationActions';
 
-class LegislationEditor extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ref: '', title: '', content: '' };
-    this.handleSubmit = this._handleSubmit.bind(this);
-    this.editLegislation = this._editLegislation.bind(this);
-    this.formatLegislation = this._formatLegislation.bind(this);
-    this.handleTextChange = this._handleTextChange.bind(this);
-    this.handleInputError = this._handleInputError.bind(this);
-    this.createPDF = this._createPDF.bind(this);
-    this.onSynced = this._onSynced.bind(this);
-    this.onReady = this._onReady.bind(this);
-    this.email = localStorage.getItem('email');
-    this.firepad = null;
-    this.originalText = '';
-    this.diffedContent = null;
-    this.formattedContent = null;
-    this.doc = new jsPDF();
-    this.pdfName = null;
-    this.inputError = false;
-    this.isEditing = false;
-  }
-  
 
-  componentDidMount() {
-    const config = {
-      apiKey: 'AIzaSyCEQ7cLJuyrbRLq_A-sjXUwxseMaKVosEE',
-      authDomain: 'turq-legislation-editor-1.firebaseapp.com',
-      databaseURL: 'https://turq-legislation-editor-1-default-rtdb.firebaseio.com',
-      projectId: 'turq-legislation-editor-1',
-      storageBucket: 'turq-legislation-editor-1.appspot.com',
-      messagingSenderId: '670875815290',
-      appId: '1:670875815290:web:a5da8f93c91052ed44dac3',
-      measurementId: 'G-Y6NX5XHT68',
-    };
-
-    if (!firebase.apps.length) {
-      firebase.initializeApp(config);
-    }
-    const firepadRef = this._createRef();
-    const codeMirror = window.CodeMirror(document.getElementById('firepad-container'), { lineWrapping: true });
-    this.firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
-      richTextShortcuts: true,
-      richTextToolbar: true,
-    });
-    this.firepad.on('ready', this.onReady);
-    this.firepad.on('synced', this.onSynced);
-  }
-
-  // Add the text to state when the page initally loads
-  _onReady() {
-    const text = this.firepad.getText();
-    this.setState({ ...this.state, content: text, originalText: text });
-    if (this.firepad.getHtml() === '') {
-      this.firepad.setHtml('<div><b>Legislation Title</b></div><div><b></b><br/></div><div><b>Section 1</b></div><div><b></b><br/></div><div><b>Section 2&nbsp;</b></div><div><b></b><br/></div><div><b>Etc.</b></div>');
-    }
-    this.setState({ ...this.state, content: text });
-  }
-
-  // Update the text stored in state everytime the user changes and text and the db syncs
-  _onSynced(isSynced) {
-    if (isSynced) {
-      const text = this.firepad.getText();
-      this.setState({ ...this.state, content: text });
-      if(this.state.isEditing) {
-        this._onDiff();
-      }
-    }
-  }
-
-  _onDiff() {
-    if(this.state.diffedContent === null) {
-      this.setState({diffedContent: this.state.originalText});
-    }
-    const dmp = new DiffMatchPatch();
-
-    const text1 = this.state.originalText;
-    const text2 = this.state.content;
-
-    const diff = dmp.diff_main(text1, text2);
-    const prettyDiff = dmp.diff_prettyHtml(diff);
-
-    this.setState({ diffedContent: prettyDiff });
-  }
-
-  // Create the reference to the firepad document -- /posts/<email>/<contestId>
-  _createRef() {
-    let ref = firebase.database().ref();
-    const URLRef = new URLSearchParams(this.props.location.search).get('ref');
-    if (URLRef) {
-      ref = ref.child(URLRef);
-    } else {
-      ref = ref.push(); // generate unique location.
-      const oldURL = window.location.origin + window.location.pathname;
-      window.history.replaceState(null, 'New Page Title', `${oldURL}?ref=${ref.key}`);
-    }
-    this.setState({ ...this.state, ref: ref.key });
-    return ref;
-  }
-
-  _checkMandatoryFields() {
-    const isValid = true;
-    return isValid;
-  }
-
-  _editLegislation() {
-    console.log("diffed content: " + this.state.diffedContent);
-    const content = this.firepad.getText();
-    if(this.state.diffedContent === undefined) {
-      this.setState({diffedContent: "begin typing in the left box to see diff"});
-    }
-    this.setState({originalText: content, isEditing: true});
-  }
-
-  _formatLegislation() {
-    const content = this.firepad.getText();
-    this.setState({formattedContent: content});
-  }
-
-  _handleTextChange = e => {
-    e.preventDefault();
-    if(this.handleInputError(e.target.value)) {
-      this.setState({inputError: true});
-    } else {
-      this.setState({inputError: false});
-    }
-    this.setState({pdfName: e.target.value}) 
-  }
-
-  _createPDF = () => {
-    this.doc.text(this.state.formattedContent, 10, 10);
-    this.doc.save(this.state.pdfName + ".pdf");
-  }
-
-  _handleInputError = (input) => {
-    return input.includes(".pdf");
-  }
-
-  _handleSubmit() {
-    if (!this._checkMandatoryFields()) {
-      return;
-    }
-    const contest = new URLSearchParams(this.props.location.search).get('contest');
-    const legislationId = this.props.match.params.id;
-    const data = {
-      title: 'Submitted Bill', ref: this.state.ref, contestId: contest, content: this.state.content,
-    };
-    this.props.dispatch(updateLegislation(legislationId, data));
-  }
-
-  render() {
+function LegislationEditor (props) {
+  const email = localStorage.getItem('email');
+  console.log(props.allState);
+  const config = {
+    docId    : "docId",
+    user     : {
+        name : email,
+    },
+    apiKey   : "7bdf58a1-e722-4868-bee9-b7e7c65a09b6",
+  };
+  const editorRef = useRef(null);
+  const log = () => {
+     if (editorRef.current) {
+       console.log(editorRef.current.getContent());
+     }
+   };
     return (
-      <EditorLayout editLegislation={this.editLegislation} onSubmit={this.handleSubmit}>
-        {!this.props.isFetching
-          ? (
-            <div>
-              <div className="row">
-                <div className="col">
-                  <Editor>
-                    <LeftPanel>
-                      <div className="my-3 mx-5">
-                        <div id="firepad-container" />
-                      </div>
-                    </LeftPanel>
-                    <RightPanel>
-                      <div className="CodeMirror mt-2 ml-2" dangerouslySetInnerHTML={{ __html: this.state.diffedContent }} />
-                    </RightPanel>
-                  </Editor>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <Button
-                    className="mb-2"
-                    variant="contained"
-                    color="secondary"
-                    onClick={this.formatLegislation}
-                  >Format Legislation</Button>
-                  <Button
-                    className="mb-2 ml-2"
-                    variant="outlined"
-                    color="secondary"
-                    onClick={this.createPDF}
-                  >Generate PDF</Button>
-                  <TextField 
-                    onChange={this.handleTextChange}
-                    value={this.state.pdfName}
-                    className="mb-2 ml-2" 
-                    helperText="no need to include .pdf, we will add it for you"
-                    error={this.state.inputError}
-                    id="filled-basic" 
-                    label="Name your PDF" 
-                    variant="filled" />
-                  <LegislationText content={this.state.formattedContent}></LegislationText>
-                </div>
-              </div>
-            </div>
-          )
-          : <div />}
+      <EditorLayout> 
+       <Editor
+         onInit={(evt, editor) => editorRef.current = editor}
+         initialValue="<p>This is the initial content of the editor.</p>"
+         apiKey="8ibbzj1vs93puli4xfo36621uu8sgesoz4kdulasuabafccp"
+         init={{
+           height: 500,
+           menubar: false,
+           plugins: 'export',
+           toolbar: 'undo redo | formatselect | ' +
+           'bold italic backcolor | alignleft aligncenter ' +
+           'alignright alignjustify | bullist numlist outdent indent | ',
+           external_plugins : {
+            wave : "https://cdn2.codox.io/waveTinymce/2.x/plugin.min.js"
+          },
+          wave   : config
+         }}
+       />
+       
       </EditorLayout>
     );
-  }
+  
 }
 
 function mapStateToProps(state) {
+  const allState = state;
   let { legislation, auth } = state;
   const { isFetching } = legislation;
   legislation = legislation.legislation;
@@ -229,6 +59,7 @@ function mapStateToProps(state) {
     isAuthenticated,
     email,
     auth,
+    allState,
   };
 }
 export default connect(mapStateToProps)(LegislationEditor);
