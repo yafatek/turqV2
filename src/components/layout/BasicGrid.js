e26fdbe26685d4f653274e5727c5323cfac63220
feraso0o0 <ceo@yafatek.dev>
2022-02-16 20:19:20 +0300

    draggable cards، v6

--- src/components/layout/BasicGrid.js
+++ src/components/layout/BasicGrid.js
@@ -4,12 +4,8 @@ import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        // flexGrow: 1,
     },
     paper: {
-        // padding: theme.spacing(2),
-        // textAlign: 'center',
-        // color: theme.palette.text.secondary,
     },
 }));
 
